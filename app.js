96c0647fc5a87d73b5ce4acc201865bd22869c86
Simon <simonpham1294@gmail.com>
2020-12-15 13:43:41 -0400

    new changes

---
+++ app.js
@@ -0,0 +1,11 @@
+const texts = ['websites', 'illustrations', 'pancakes'];
+let count = 0; 
+let index = 0; 
+let currentText = ''; 
+let letter = ''; 
+
+(function type() {
+    if(count === texts.length) {
+        count = 0; 
+    }
+}());
