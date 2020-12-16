5bbfac23308f56054ad5a913c0ad9b5ebdfbb0f9
Simon <simonpham1294@gmail.com>
2020-12-15 20:23:19 -0400

    new changes

--- app.js
+++ app.js
@@ -1,11 +1,70 @@
-const texts = ['websites', 'illustrations', 'pancakes'];
-let count = 0; 
-let index = 0; 
-let currentText = ''; 
-let letter = ''; 
 
-(function type() {
-    if(count === texts.length) {
-        count = 0; 
-    }
-}());
+// const text = 'and I make beautiful cakes';
+// const extra = document.querySelector('.extra'); 
+// let introText = '';
+// let i = 0;
+// let j = 0;
+
+
+// function type() {
+//     if(i < text.length) {
+//         introText = document.querySelector('.typing').innerHTML += text.charAt(i);
+//         i++;
+//         setTimeout(type, 100);
+//     } 
+    
+    
+// };
+
+
+// function erase() {
+//     type();
+//     introText = introText.substring(0, introText.length-1);
+//     introText--;
+//     setTimeout(erase, 2000);
+// }
+
+// erase()
+
+
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = [" A Front-end Web Developer", " A Hufflepuff"];
+const typingDelay = 150;
+const erasingDelay = 50;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
